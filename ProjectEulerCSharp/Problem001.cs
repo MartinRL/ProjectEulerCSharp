ad573c49d8b367f83211e6c2832e65c07c6b1fbe
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-05 21:53:15 +0200

    Solved problem 1

---
+++ ProjectEulerCSharp/Problem001.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit.Extensions;
+
+namespace ProjectEulerCSharp
+{
+    public class Problem001
+    {
+        [Theory]
+        [InlineData(9, 23)]
+        [InlineData(999, 233168)]
+        public void should_find_sum_of_multiples_of_3_or_5_for_upper_bound(int upperBound, int expectedSum)
+        {
+            var actualSum = 0.To(upperBound)
+                .Where(i => i.IsEvenlyDivisibleBy(3)
+                         || i.IsEvenlyDivisibleBy(5))
+                .Sum();
+
+            actualSum.Should().Be(expectedSum);
+        }
+    }
+}

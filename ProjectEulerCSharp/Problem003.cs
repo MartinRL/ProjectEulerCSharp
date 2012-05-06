dd4f95d5228580fba69b567205a37dc1918ca36a
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-06 21:29:41 +0200

    green :)

--- ProjectEulerCSharp/Problem003.cs
+++ ProjectEulerCSharp/Problem003.cs
@@ -1,23 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
-using Xunit;
 using Xunit.Extensions;
 
 namespace ProjectEulerCSharp
 {
     public class Problem003
     {
-        [Fact]
-        public void dummy()
-        {
-            long i = 600851475143;
-        }
-
         [Theory]
         [InlineData(13195, 29)]
-        [InlineData(600851475143, 0)]
-        public void should_find_largest_prime_factor( long term, int expectedLargestPrimeFactor )
+        [InlineData(600851475143, 6857)]
+        public void should_find_largest_prime_factor(long term, int expectedLargestPrimeFactor)
         {
-            var actualLargestPrimeFactor = 0;
+            var actualLargestPrimeFactor = term.CalculateMaxFactor().ToMin()
+                .First(t => t.IsPrime() && term.IsEvenlyDivisibleBy(t));
 
             actualLargestPrimeFactor.Should().Be(expectedLargestPrimeFactor);
         }

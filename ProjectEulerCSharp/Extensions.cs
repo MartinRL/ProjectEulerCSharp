46941c3d0a3fc68c9079008e9f7b99c3bef8a765
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-06 20:39:06 +0200

    solved problem 2

--- ProjectEulerCSharp/Extensions.cs
+++ ProjectEulerCSharp/Extensions.cs
@@ -5,14 +5,29 @@ namespace ProjectEulerCSharp
 {
     public static class Extensions
     {
+        public static IEnumerable<int> ToMax(this int @this)
+        {
+            return @this.To(int.MaxValue);
+        }
+
         public static IEnumerable<int> To(this int @this, int to)
         {
             return Enumerable.Range(@this, to - @this + 1);
         }
 
+        public static bool IsEven( this int @this )
+        {
+            return @this.IsEvenlyDivisibleBy(2);
+        }
+
         public static bool IsEvenlyDivisibleBy(this int @this, int divisor)
         {
             return @this % divisor == 0;
+        }
+
+        public static IEnumerable<int> TakeWhileLessThan(this IEnumerable<int> @this, int upperBound)
+        {
+            return @this.TakeWhile(term => term <= upperBound);
         }
     }
 }

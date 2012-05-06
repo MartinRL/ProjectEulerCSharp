06dfdd161be902a49cb3327289b9fd28896d16f9
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-06 18:26:23 +0200

    rf: move to file

--- ProjectEulerCSharp/Problem002.cs
+++ ProjectEulerCSharp/Problem002.cs
@@ -1,8 +1,7 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using FluentAssertions;
 using System.Linq;
+using ProjectEulerCSharp.Sequences;
 using Xunit;
 using Xunit.Extensions;
 
@@ -21,34 +20,6 @@ namespace ProjectEulerCSharp
                 .Sum();
 
             actualSum.Should().Be(expectedSum);
-        }
-    }
-
-    public class FibonacciSequence : IEnumerable<int>
-    {
-        private readonly IEnumerable<int> sequence;
-
-        public FibonacciSequence()
-        {
-            sequence = 1.To(int.MaxValue).Select(CalculateFibonacci);
-        }
-
-        private int CalculateFibonacci(int index)
-        {
-            if (index <= 1)
-                return 1;
-
-            return CalculateFibonacci(index - 1) + CalculateFibonacci(index - 2);
-        }
-
-        public IEnumerator<int> GetEnumerator()
-        {
-            return sequence.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
         }
     }
 }
---
+++ ProjectEulerCSharp/Sequences/FibonacciSequence.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEulerCSharp.Sequences
+{
+    public class FibonacciSequence : IEnumerable<int>
+    {
+        private readonly IEnumerable<int> sequence;
+
+        public FibonacciSequence()
+        {
+            sequence = 1.To(int.MaxValue).Select(CalculateFibonacci);
+        }
+
+        private int CalculateFibonacci(int index)
+        {
+            if (index <= 1)
+                return 1;
+
+            return CalculateFibonacci(index - 1) + CalculateFibonacci(index - 2);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return sequence.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}

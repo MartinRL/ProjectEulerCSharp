c9390bbeabc33b3ddf5c5805acba1c7714714bf5
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-06 21:30:25 +0200

    solved problem 3

---
+++ ProjectEulerCSharp/Problem003.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit.Extensions;
+
+namespace ProjectEulerCSharp
+{
+    public class Problem003
+    {
+        [Theory]
+        [InlineData(13195, 29)]
+        [InlineData(600851475143, 6857)]
+        public void should_find_largest_prime_factor(long term, int expectedLargestPrimeFactor)
+        {
+            var actualLargestPrimeFactor = term.CalculateMaxFactor().ToMin()
+                .First(t => t.IsPrime() && term.IsEvenlyDivisibleBy(t));
+
+            actualLargestPrimeFactor.Should().Be(expectedLargestPrimeFactor);
+        }
+    }
+}

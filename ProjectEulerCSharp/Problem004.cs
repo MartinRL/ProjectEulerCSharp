84015a30febf1ac6b69626cd5b776db7b5b691b4
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-07 22:32:05 +0200

    green :)

---
+++ ProjectEulerCSharp/Problem004.cs
@@ -0,0 +1,31 @@
+using FluentAssertions;
+using Xunit;
+using Xunit.Extensions;
+using System.Linq;
+
+namespace ProjectEulerCSharp
+{
+    public class Problem004
+    {
+        [Theory]
+        [InlineData(99, 9009)]
+        [InlineData(999, 906609)]
+        public void should_find_largest_palindromic_number(int maxFactor, int expectedPalindromicNumber)
+        {
+            var firstFactorSequence = 0.To(maxFactor);
+            var secondFactorSequence = 0.To(maxFactor);
+
+            var actualPalindromicNumber = firstFactorSequence
+                .SelectMany(firstFactor => secondFactorSequence.Select(secondFactor => firstFactor * secondFactor))
+                .Where(IsPalindrome)
+                .Max();
+
+            actualPalindromicNumber.Should().Be(expectedPalindromicNumber);
+        }
+
+        private static bool IsPalindrome(int term)
+        {
+            return term.ToString().SequenceEqual(term.ToString().Reverse());
+        }
+    }
+}

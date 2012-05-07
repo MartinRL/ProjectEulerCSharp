cbe82fa63e135fa81c17f0edc54b2fc997fb72b6
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-07 22:52:47 +0200

    removed finder class. too much noise.

--- ProjectEulerCSharp/Problem001.cs
+++ ProjectEulerCSharp/Problem001.cs
@@ -12,29 +12,12 @@ namespace ProjectEulerCSharp
         [InlineData(999, 233168)]
         public void should_find_sum_of_multiples_of_3_or_5_for_upper_bound(int upperBound, int expectedSum)
         {
-            var finder = new MultiplesOf3or5Finder(upperBound);
-
-            var actualSum = finder.Find();
+            var actualSum = 0.To(upperBound)
+                .Where(i => i.IsEvenlyDivisibleBy(3)
+                         || i.IsEvenlyDivisibleBy(5))
+                .Sum();
 
             actualSum.Should().Be(expectedSum);
-        }
-
-        public class MultiplesOf3or5Finder
-        {
-            private readonly int upperBound;
-
-            public MultiplesOf3or5Finder(int upperBound)
-            {
-                this.upperBound = upperBound;
-            }
-
-            public int Find()
-            {
-                return 0.To(upperBound)
-                    .Where(i => i.IsEvenlyDivisibleBy(3)
-                             || i.IsEvenlyDivisibleBy(5))
-                    .Sum();
-            }
         }
     }
 }

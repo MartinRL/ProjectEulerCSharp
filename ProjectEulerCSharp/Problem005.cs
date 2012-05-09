525fa7c25a17987c55aeffb564c33e9055da1d56
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-09 21:41:52 +0200

    fixed typo

--- ProjectEulerCSharp/Problem005.cs
+++ ProjectEulerCSharp/Problem005.cs
@@ -9,7 +9,7 @@ namespace ProjectEulerCSharp
         [Theory]
         [InlineData(10, 2520)]
         [InlineData(20, 232792560)]
-        public void should_find_smallest_positive_number_evenly_divisible_divisors(int upperBound, int expected)
+        public void should_find_smallest_positive_number_evenly_divisible_by_divisors(int upperBound, int expected)
         {
             var divisors = 1.To(upperBound);
 

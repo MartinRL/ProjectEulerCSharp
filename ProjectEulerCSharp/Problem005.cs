e080503f9426a4018db71f108b946540607e0f73
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-09 20:52:16 +0200

    green

---
+++ ProjectEulerCSharp/Problem005.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using FluentAssertions;
+using Xunit.Extensions;
+
+namespace ProjectEulerCSharp
+{
+    public class Problem005
+    {
+        [Theory]
+        [InlineData(10, 2520)]
+        [InlineData(20, 232792560)]
+        public void should_find_smallest_positive_number_evenly_divisible_divisors(int upperBound, int expected)
+        {
+            var divisors = 1.To(upperBound);
+
+            var actual = upperBound.ToMax()
+                .Where(term => term.IsEven())
+                .First(term => divisors.All(divisor => term.IsEvenlyDivisibleBy(divisor)));
+
+            actual.Should().Be(expected);
+        }
+    }
+}

656a2d69a5116f42c89ddcbd595f7d9fac0261e4
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-09 20:26:28 +0200

    added dummy fact

---
+++ ProjectEulerCSharp/NCrunchDummyFact.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace ProjectEulerCSharp
+{
+    /// <summary>
+    /// As of now, NCrunch unfortunately won't execute a test suite with only theories, hence this dummy fact.
+    /// </summary>
+    public class NCrunchDummyFact
+    {
+        [Fact]
+        public void dummy()
+        {
+        }
+    }
+}

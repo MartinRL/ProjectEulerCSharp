d445bc4f5b3440b96491d11027c488fd12341b1a
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-10 21:31:50 +0200

    added comment that might help

---
+++ ProjectEulerCSharp/Problem008.cs
@@ -0,0 +1,7 @@
+namespace ProjectEulerCSharp
+{
+    public class Problem008
+    {
+        // http://dotnetslackers.com/XLinq/re-242129_Use_LINQ_Aggregate_to_Multiply_a_Series_of_Digits.aspx
+    }
+}

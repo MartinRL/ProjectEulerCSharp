a7ceadc83cb55ce72a274c42cd9629872d604927
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-10 21:11:57 +0200

    green :)

---
+++ ProjectEulerCSharp/Problem006.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using FluentAssertions;
+using Xunit.Extensions;
+
+namespace ProjectEulerCSharp
+{
+    public class Problem006
+    {
+        [Theory]
+        [InlineData(10, 2640)]
+        [InlineData(100, 25164150)]
+        public void should_find_the_difference_between_the_sum_of_the_squares_and_the_square_of_the_sum(int upperBound, int expected)
+        {
+            var naturalNumbers = 1.To(upperBound);
+
+            var sumOfTheSquares = naturalNumbers.Select(t => t.Sqr()).Sum();
+
+            var squareOfTheSums = naturalNumbers.Sum().Sqr();
+
+            var actual = squareOfTheSums - sumOfTheSquares;
+
+            actual.Should().Be(expected);
+        }
+    }
+}

b556ffbe54dc57414b18a4721c68b6de63b3460c
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-14 20:38:44 +0200

    red: added failing theory

--- ProjectEulerCSharp/Problem010.cs
+++ ProjectEulerCSharp/Problem010.cs
@@ -8,6 +8,7 @@ namespace ProjectEulerCSharp
     {
         [Theory]
         [InlineData(10, 17)]
+        [InlineData(2000000, 1)]
         public void find_the_sum_of_all_the_primes(int upperBound, int expected)
         {
             1.To(upperBound)

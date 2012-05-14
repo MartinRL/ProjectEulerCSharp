c87063bd27a8ac84366862f9a26e8d1ff7c09210
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-05-14 20:53:07 +0200

    green

--- ProjectEulerCSharp/Problem010.cs
+++ ProjectEulerCSharp/Problem010.cs
@@ -7,9 +7,9 @@ namespace ProjectEulerCSharp
     public class Problem010
     {
         [Theory]
-        [InlineData(10, 17)]
-        [InlineData(2000000, 1)]
-        public void find_the_sum_of_all_the_primes(int upperBound, int expected)
+        [InlineData((long)10, (long)17)]
+        [InlineData((long)2000000, 142913828922)]
+        public void should_find_the_sum_of_all_the_primes(long upperBound, long expected)
         {
             1.To(upperBound)
                 .Where(t => t.IsPrime())

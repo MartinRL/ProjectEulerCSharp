b5c0fe8c16aaabe95be58010cd00ea97dbb794a7
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-07-10 21:19:24 +0200

    skipping IEnumerable overhead. not interested in the sequence, only the count.

--- ProjectEulerCSharp/Problem014.cs
+++ ProjectEulerCSharp/Problem014.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using Xunit;
+using Xunit.Extensions;
 
 namespace ProjectEulerCSharp
 {
@@ -12,97 +13,33 @@ namespace ProjectEulerCSharp
         [Fact]
         public void should_find_the_starting_number_less_than_1000000_that_produces_the_longest_chain()
         {
-            1.To(1000000 - 1)
-                .Select(t => new {Term = t, Count = new CollatzSequence(t).Count})
-                .OrderByDescending(tc => tc.Count)
-                .First().Term
-                .Should().Be(0);
-        }
+            uint startingNumberOfLongestChain = 0;
+            ulong longestChainCount = 0;
 
-        [Fact]
-        public void should_find_10_as_count_for_13_as_seed()
-        {
-            var collatzSequence = new CollatzSequence(13);
-
-            collatzSequence.Count.Should().Be(10);
-        }
-    }
-
-    public class CollatzSequence : IEnumerable<int>
-    {
-        private readonly int seed;
-
-        public CollatzSequence(int seed)
-        {
-            this.seed = seed;
-        }
-
-        public ulong Count
-        {
-            get
+            for (uint i = 1; i < 1000000; i++)
             {
-                ulong count = 0;
-                var enumerator = GetEnumerator();
-                while (enumerator.MoveNext())
+                var member = i;
+                ulong chainCount = 0;
+
+                while (member != 1)
                 {
-                    count++;
+                    if (member % 2 == 0)
+                        member = member / 2;
+                    else
+                        member = 3 * member + 1;
+
+                    chainCount++;
                 }
 
-                return count;
-            }
-        }
-
-        public IEnumerator<int> GetEnumerator()
-        {
-            return new Enumerator(seed);
-        }
-
-        public class Enumerator : IEnumerator<int>
-        {
-            private int current;
-            private bool hasMoved;
-
-            public Enumerator(int current)
-            {
-                hasMoved = false;
-                this.current = current;
+                if (longestChainCount < chainCount)
+                {
+                    longestChainCount = chainCount;
+                    startingNumberOfLongestChain = i;
+                }
             }
 
-            public void Dispose()
-            {}
-
-            public bool MoveNext()
-            {
-                if (Current == 1)
-                    return false;
-
-                if (hasMoved)
-                    current = current.IsEven() ? current / 2 : 3 * current + 1;
-
-                hasMoved = true;
-
-                return true;
-            }
-
-            public void Reset()
-            {
-                throw new NotImplementedException();
-            }
-
-            public int Current
-            {
-                get { return current; }
-            }
-
-            object IEnumerator.Current
-            {
-                get { return Current; }
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
+            startingNumberOfLongestChain.Should().Be(837799);
+            longestChainCount.Should().Be(524);
         }
     }
 }

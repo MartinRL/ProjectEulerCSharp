fcad724ddfa72b4b2de6095cad88ddb82a91b280
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-07-11 21:10:05 +0200

    rf: using recursive method. loosing some performance, but like the loss of looping code.

--- ProjectEulerCSharp/Problem014.cs
+++ ProjectEulerCSharp/Problem014.cs
@@ -14,7 +14,7 @@ namespace ProjectEulerCSharp
         public void should_find_the_starting_number_less_than_1000000_that_produces_the_longest_chain()
         {
             var result = 2.ToUint(999999)
-                .Select(t => new {Term = t, Count = GetChainCountForR(t)})
+                .Select(t => new {Term = t, Count = GetChainCountFor(t)})
                 .OrderByDescending(tc => tc.Count)
                 .First();
 
@@ -22,28 +22,12 @@ namespace ProjectEulerCSharp
             result.Count.Should().Be(524);
         }
 
-        private static ulong GetChainCountForR(uint startingNumber, ulong count = (ulong)0)
+        private static ulong GetChainCountFor(uint startingNumber, ulong count = (ulong)0)
         {
             if (startingNumber == 1)
                 return count;
 
-            return GetChainCountForR(startingNumber % 2 == 0 ? startingNumber / 2 : 3 * startingNumber + 1, ++count);
-        }
-
-        private static ulong GetChainCountFor(uint startingNumber)
-        {
-            ulong chainCount = 0;
-            while (startingNumber != 1)
-            {
-                if (startingNumber % 2 == 0)
-                    startingNumber = startingNumber / 2;
-                else
-                    startingNumber = 3 * startingNumber + 1;
-
-                chainCount++;
-            }
-
-            return chainCount;
+            return GetChainCountFor(startingNumber % 2 == 0 ? startingNumber / 2 : 3 * startingNumber + 1, ++count);
         }
     }
 }

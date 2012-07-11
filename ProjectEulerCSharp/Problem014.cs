ea4706623662ef0516189e8b76aeae31b12e86bf
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-07-11 21:08:04 +0200

    rf: trying recursive count method

--- ProjectEulerCSharp/Problem014.cs
+++ ProjectEulerCSharp/Problem014.cs
@@ -13,8 +13,8 @@ namespace ProjectEulerCSharp
         [Fact]
         public void should_find_the_starting_number_less_than_1000000_that_produces_the_longest_chain()
         {
-            var result = 1.ToUint(999999)
-                .Select(t => new {Term = t, Count = GetChainCountFor(t)})
+            var result = 2.ToUint(999999)
+                .Select(t => new {Term = t, Count = GetChainCountForR(t)})
                 .OrderByDescending(tc => tc.Count)
                 .First();
 
@@ -22,10 +22,17 @@ namespace ProjectEulerCSharp
             result.Count.Should().Be(524);
         }
 
+        private static ulong GetChainCountForR(uint startingNumber, ulong count = (ulong)0)
+        {
+            if (startingNumber == 1)
+                return count;
+
+            return GetChainCountForR(startingNumber % 2 == 0 ? startingNumber / 2 : 3 * startingNumber + 1, ++count);
+        }
+
         private static ulong GetChainCountFor(uint startingNumber)
         {
             ulong chainCount = 0;
-
             while (startingNumber != 1)
             {
                 if (startingNumber % 2 == 0)
@@ -35,6 +42,7 @@ namespace ProjectEulerCSharp
 
                 chainCount++;
             }
+
             return chainCount;
         }
     }

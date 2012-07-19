02629d800ec96a493c94bae30e288b443fb9a673
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-07-19 20:27:03 +0200

    rf: remove unused usings

--- ProjectEulerCSharp/Problem014.cs
+++ ProjectEulerCSharp/Problem014.cs
@@ -1,10 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using Xunit;
-using Xunit.Extensions;
 
 namespace ProjectEulerCSharp
 {

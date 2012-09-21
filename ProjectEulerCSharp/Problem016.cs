5a5fafd89e891aa5a642075269b55e8c0ad6de0d
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-09-21 17:35:05 +0200

    add empty unit test

---
+++ ProjectEulerCSharp/Problem016.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ProjectEulerCSharp
+{
+    public class Problem016
+    {
+        [Fact]
+        public void should_find_he_sum_of_the_digits_of_2_raised_to_the_power_of_1000()
+        {
+
+        }
+    }
+}

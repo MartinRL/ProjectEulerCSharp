543173903ebf3b1322a521d1b1bb9480f74ffcb6
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-09-22 16:16:31 +0200

    add empty fact

---
+++ ProjectEulerCSharp/Problem017.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ProjectEulerCSharp
+{
+    public class Problem017
+    {
+        [Fact]
+        public void should_find_the_number_of_letters_of_1_to_1000_written_out_in_words()
+        {
+        }
+    }
+}
